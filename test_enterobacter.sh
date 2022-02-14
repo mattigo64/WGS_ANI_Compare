b38bffa9800a709e08e3ea9dfc2b5860bd89d026
mattigo64 <69928398+mattigo64@users.noreply.github.com>
2022-02-14 12:45:45 -0500

    Add files via upload

---
+++ test_enterobacter.sh
@@ -0,0 +1,35 @@
+
+id=("ASM1904710v1", "ASM1602849v1")
+
+
+#for FAST in FASTA 
+#DO
+#esearch -db nuccore -query $id | efetch -format fasta > seq.fa
+#FINISH
+
+#for i in `$id`; do efetch -db nuccore -id ${i} -format fasta > ${i}.fa ; done
+
+#epost -db nuccore -input id -format acc | efetch -format fasta | grep ">"
+
+
+#for FAST in $id
+#do
+#	esearch -db nuccore -query ${FAST} | efetch -format fasta > ${FAST}.fa
+#done
+
+
+for FAST in "${id[@]}"
+do
+    esearch -db assembly -query $FAST </dev/null \
+        | esummary \
+        | xtract -pattern DocumentSummary -element FtpPath_GenBank \
+        | while read -r url ; do
+            fname=$(echo $url | grep -o 'GCA_.*' | sed 's/$/_genomic.fna.gz/') ;
+            wget "$url/$fname" ;
+        done ;
+    done
+
+
+
+echo "DONE"
+
